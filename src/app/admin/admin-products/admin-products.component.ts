9417eed8a4a70e15d081a17dbf541b1a847f55fc
Evren Ceyhan <evren@ceyhan.io>
2020-05-21 23:52:52 +0200

    product filtering impl

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { ProductService } from '../../product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
     selector: 'app-admin-products',
     templateUrl: './admin-products.component.html',
     styleUrls: ['./admin-products.component.css'],
 })
-export class AdminProductsComponent implements OnInit {
-    products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
+    sub: Subscription;
 
     constructor(private productSvc: ProductService) {}
 
     ngOnInit(): void {
-        this.products$ = this.productSvc.getAll();
+        this.sub = this.productSvc
+            .getAll()
+            .subscribe(
+                (products) => (this.filteredProducts = this.products = products)
+            );
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     onDelete(id: string) {
@@ -22,5 +34,11 @@ export class AdminProductsComponent implements OnInit {
         }
         // prevent default href event
         return false;
+    }
+
+    onFilter(query: string) {
+        this.filteredProducts = query
+            ? this.products.filter((p) => p.title.toLowerCase().includes(query.toLowerCase()))
+            : this.products;
     }
 }
