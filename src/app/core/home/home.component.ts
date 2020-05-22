b39a7cd5611dd1eac499e7d291f96e0f048bf934
Evren Ceyhan <evren@ceyhan.io>
2020-05-22 14:02:26 +0200

    category links created

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CategoryService } from 'src/app/shared/services/category.service';
+import { Category } from 'src/app/shared/models/category';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+    categories$: Observable<Category[]>;
 
-  constructor() { }
+    constructor(private categorySvc: CategoryService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.categories$ = this.categorySvc.getAll();
+    }
 }
---
+++ src/app/shared/models/item.ts
@@ -0,0 +1,6 @@
+export interface Item {
+    id?: string;
+    name?: string;
+    value?: string;
+    label?: string;
+}
