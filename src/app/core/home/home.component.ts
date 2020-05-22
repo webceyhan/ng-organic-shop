ca5d2bbf1a5d6ec5d2fbb0623704106654fdc032
Evren Ceyhan <evren@ceyhan.io>
2020-05-22 18:48:04 +0200

    product cards moved into products component

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
-
-import { ProductService } from 'src/app/shared/services/product.service';
-import { Product } from 'src/app/shared/models/product';
 
 @Component({
     selector: 'app-home',
@@ -12,44 +6,7 @@ import { Product } from 'src/app/shared/models/product';
     styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-    // products$: Observable<Product[]>;
+    constructor() {}
 
-    products: Product[] = [];
-    filteredProducts$: Observable<Product[]>;
-
-    constructor(
-        private route: ActivatedRoute,
-        private productSvc: ProductService
-    ) {}
-
-    ngOnInit(): void {
-        //
-        // server-side filtering approach
-        //
-
-        // this.products$ = this.route.queryParams.pipe(
-        //     map((params) => params.category),
-        //     switchMap((category) => {
-        //         return this.productSvc.getAll((ref) =>
-        //             category
-        //                 ? ref.orderByChild('category').equalTo(category)
-        //                 : ref
-        //         );
-        //     })
-        // );
-
-        //
-        // client-side filtering
-        //
-        this.filteredProducts$ = this.productSvc.getAll().pipe(
-            tap((all) => (this.products = all)),
-            switchMap(() => this.route.queryParams),
-            map((params) => params.category),
-            map((category) =>
-                category
-                    ? this.products.filter((p) => p.category === category)
-                    : this.products
-            )
-        );
-    }
+    ngOnInit(): void {}
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap, switchMap, map } from 'rxjs/operators';
+
+import { Product } from '../shared/models/product';
+import { ProductService } from '../shared/services/product.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+    // products$: Observable<Product[]>;
 
-  constructor() { }
+    products: Product[] = [];
+    filteredProducts$: Observable<Product[]>;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private productSvc: ProductService
+    ) {}
 
+    ngOnInit(): void {
+        //
+        // server-side filtering approach
+        //
+
+        // this.products$ = this.route.queryParams.pipe(
+        //     map((params) => params.category),
+        //     switchMap((category) => {
+        //         return this.productSvc.getAll((ref) =>
+        //             category
+        //                 ? ref.orderByChild('category').equalTo(category)
+        //                 : ref
+        //         );
+        //     })
+        // );
+
+        //
+        // client-side filtering
+        //
+        this.filteredProducts$ = this.productSvc.getAll().pipe(
+            tap((all) => (this.products = all)),
+            switchMap(() => this.route.queryParams),
+            map((params) => params.category),
+            map((category) =>
+                category
+                    ? this.products.filter((p) => p.category === category)
+                    : this.products
+            )
+        );
+    }
 }
