012e9d9af70edce5d84fe4fe10723eeb4f7847e4
Evren Ceyhan <evren@ceyhan.io>
2020-05-23 02:38:39 +0200

    basket items impl

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BasketService } from '../shared/services/basket.service';
+import { BasketItem } from '../shared/models/basket';
 
 @Component({
-  selector: 'app-basket',
-  templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.css']
+    selector: 'app-basket',
+    templateUrl: './basket.component.html',
+    styleUrls: ['./basket.component.css'],
 })
 export class BasketComponent implements OnInit {
+    items$: Observable<BasketItem[]>;
+    itemCount$: Observable<number>;
+    total$: Observable<number>;
 
-  constructor() { }
+    constructor(private basketSvc: BasketService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.itemCount$ = this.basketSvc.count$;
+        this.items$ = this.basketSvc.getAll();
+        this.total$ = this.basketSvc.total$;
+    }
 
+    onClear() {
+        this.basketSvc.clear();
+    }
 }
