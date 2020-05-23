4dc90d100871ec8a4b649d92c7d4ffd80123e4be
Evren Ceyhan <evren@ceyhan.io>
2020-05-23 14:07:33 +0200

    ng-bootstrap moved to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
@@ -33,13 +32,7 @@ import { ProductCardComponent } from './products/product-card/product-card.compo
         ProductFilterComponent,
         ProductCardComponent,
     ],
-    imports: [
-        BrowserModule,
-        NgbModule,
-        CoreModule,
-        SharedModule,
-        AppRoutingModule,
-    ],
+    imports: [BrowserModule, CoreModule, SharedModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { UrlValidator } from './validators/url.validator';
 import { MinValidator } from './validators/min.validator';
@@ -9,7 +10,7 @@ const DECLARATIONS = [UrlValidator, MinValidator];
 
 @NgModule({
     declarations: DECLARATIONS,
-    imports: [CommonModule, FormsModule],
-    exports: [CommonModule, FormsModule, ...DECLARATIONS],
+    imports: [CommonModule, FormsModule, NgbModule],
+    exports: [CommonModule, FormsModule, NgbModule, ...DECLARATIONS],
 })
 export class SharedModule {}
