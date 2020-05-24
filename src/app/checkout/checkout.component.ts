1789561a7207813ee8e88d610d0a8bb21bdc2dd9
Evren Ceyhan <evren@ceyhan.io>
2020-05-24 22:23:53 +0200

    checkout refactored

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from '../shared/services/basket.service';
+import { Observable } from 'rxjs';
+import { BasketItem } from '../shared/models/basket';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+    selector: 'app-checkout',
+    templateUrl: './checkout.component.html',
+    styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+    items$: Observable<BasketItem[]>;
+    total$: Observable<number>;
 
-  constructor() { }
+    constructor(private basketSvc: BasketService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.items$ = this.basketSvc.getItems()
+        this.total$ = this.basketSvc.total$;
+    }
 }
