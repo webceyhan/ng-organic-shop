4618e1078d823f668890c1ee65e140618c886493
Evren Ceyhan <evren@ceyhan.io>
2020-05-26 19:07:55 +0200

    logic cleanup

--- src/app/shopping/orders/orders.component.ts
+++ src/app/shopping/orders/orders.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { switchMap, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 import { AuthService } from 'shared/services/auth.service';
 import { OrderService } from 'shared/services/order.service';
 import { Order } from 'shared/models/order';
-import { User } from 'shared/models/user';
 
 @Component({
     selector: 'app-orders',
@@ -13,18 +12,14 @@ import { User } from 'shared/models/user';
     styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-    user: User;
     orders$: Observable<Order[]>;
 
     constructor(private authSvc: AuthService, private orderSvc: OrderService) {}
 
     ngOnInit(): void {
         this.orders$ = this.authSvc.user$.pipe(
-            tap((user) => (this.user = user)),
-            switchMap((user) =>
-                user ? this.orderSvc.listByUser(user.id) : of(null)
-            ),
-            map((orders) => orders as Order[])
+            map((user) => user.id),
+            switchMap((id) => this.orderSvc.listByUser(id))
         );
     }
 }
