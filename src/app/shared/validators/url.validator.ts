f13edda2d87bf86503605750f5fafeb62478b767
Evren Ceyhan <evren@ceyhan.io>
2020-05-27 18:34:14 +0200

    product image url made none-required

--- src/app/shared/validators/url.validator.ts
+++ src/app/shared/validators/url.validator.ts
@@ -11,6 +11,7 @@ export const url: ValidatorFn = (
     control: AbstractControl
 ): ValidationErrors => {
     const pattern = /^((((http[s]?):\/{2})?)+(([0-9a-z_-]+\.)+([a-z]{2,3}))(:[0-9]+)?((\/([~0-9a-zA-Z\#\+\%@\.\/_-]+))?(\?[0-9a-zA-Z\+\%@\/&\[\];=_-]+)?)?)/;
+     if (!control.value || control.value === '') return null;
     return pattern.test(control.value) ? null : { url: true };
 };
 
